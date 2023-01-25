117256ec076189b82235b711aca6cae7ca5e70ce
fernando predes <fernando_predes@hotmail.com>
2023-01-25 11:25:27 -0300

    adicionadas as ondas

--- src/components/HeroSection/HeroSection.style.ts
+++ src/components/HeroSection/HeroSection.style.ts
@@ -16,7 +16,10 @@ export const HeroStyle = styled.div`
 
 
     .main-text{
+      position: relative;
+      z-index: 999;
       margin: auto 0;
+      padding-bottom: 10rem;
       h1{
         font-style: normal;
         font-weight: 100;
@@ -36,6 +39,71 @@ export const HeroStyle = styled.div`
     }
 
     .bottle{
+      position: relative;
+      z-index: 999;
       margin-bottom: 55px;
     }
+    .waves-container{
+      bottom: 0vh;
+      position: absolute;
+      z-index: 0;
+    }
+    .waves {
+  position:relative;
+  width: 1191px;
+  height:25vh;
+  margin-bottom:-7px; /*Fix for safari gap*/
+  min-height:100px;
+  max-height:265px;
+}
+
+.content {
+  position:relative;
+  height:20vh;
+  text-align:center;
+  background-color: white;
+}
+
+/* Animation */
+
+.parallax > use {
+  animation: move-forever 25s cubic-bezier(.55,.5,.45,.5)     infinite;
+}
+.parallax > use:nth-child(1) {
+  animation-delay: -2s;
+  animation-duration: 7s;
+}
+.parallax > use:nth-child(2) {
+  animation-delay: -3s;
+  animation-duration: 10s;
+}
+.parallax > use:nth-child(3) {
+  animation-delay: -4s;
+  animation-duration: 13s;
+}
+.parallax > use:nth-child(4) {
+  animation-delay: -5s;
+  animation-duration: 20s;
+}
+@keyframes move-forever {
+  0% {
+   transform: translate3d(-90px,0,0);
+  }
+  100% {
+    transform: translate3d(85px,0,0);
+  }
+}
+/*Shrinking for mobile*/
+@media (max-width: 768px) {
+  .waves {
+    height:40px;
+    min-height:40px;
+  }
+  .content {
+    height:30vh;
+  }
+  h1 {
+    font-size:24px;
+  }
+}
   `
--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -14,6 +14,22 @@ const HeroSection = () => {
       <div>
         <img className='bottle' src={bottle} alt="garrafa de hidromel" />
       </div>
+
+      <div className='waves-container'>
+      <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+      <defs>
+      <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+      </defs>
+      <g className="parallax">
+      <use href="#gentle-wave" x="48" y="0" fill="rgba(193,149,103,0.7" />
+      <use href="#gentle-wave" x="48" y="3" fill="rgba(193,149,103,0.5)" />
+      <use href="#gentle-wave" x="48" y="5" fill="rgba(193,149,103,0.3)" />
+      <use href="#gentle-wave" x="48" y="7" fill="#C19567" />
+      </g>
+      </svg>
+      </div>
+
     </ HeroStyle>
   )
 }
