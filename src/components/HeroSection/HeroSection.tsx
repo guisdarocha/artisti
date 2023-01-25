c17cee4bcd451f258e91ac205590dcaf50a5e133
fernando predes <fernando_predes@hotmail.com>
2023-01-25 00:25:25 -0300

    finalizado o CSS inicial

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,11 +1,19 @@
 import { HeroStyle } from './HeroSection.style'
 import bottle from '../../assets/hidromelsemfundo.png'
+import key from "../../assets/chave.png"
 
 
 const HeroSection = () => {
   return (
-    <HeroStyle className='container'>
-      <img className='bottle' src={bottle} alt="garrafa de hidromel" />
+    <HeroStyle>
+      <div className='main-text'>
+        <img src={key} alt="imagem de chave" />
+        <h1>O Hidromel Brasileiro</h1>
+        <h2>Produzido com mel orgânico.</h2>
+      </div>
+      <div>
+        <img className='bottle' src={bottle} alt="garrafa de hidromel" />
+      </div>
     </ HeroStyle>
   )
 }
