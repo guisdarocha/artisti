d4edf25c0e639bfddd4a3387f46867f9726b2af9
Guilherme da Rocha <guis.darocha@gmail.com>
2023-01-26 20:28:03 -0300

    Started the about section

---
+++ src/components/about/About.style.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const AboutStyle = styled.div`
+
+.container{
+    display: flex;
+    width: 1039px;
+    height: 724px;
+}
+
+.quemsomos{
+ img{
+    
+ }
+}
+
+
+
+h3{
+    
+    font-weight: 100;
+    font-size: 50px;
+    line-height: 75px;
+    letter-spacing: 0.16em;
+}
+
+
+`
---
+++ src/components/about/About.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { AboutStyle } from "./About.style"
+import chave from "../../assets/chave.png"
+import quemsomos from "../../assets/quemsomos.png"
+
+
+type AboutProps = {}
+
+const About = (props: AboutProps) => {
+  return (
+    <AboutStyle>
+     <div className="container">
+      <img src={quemsomos} alt=""/>
+        <div className="sobrenos">
+          <img src={chave} alt="" className='chave' />
+          <h3>Sobre nós</h3>
+          <p>A Artisti é uma iniciativa de dois jovens estudantes de enologia da Serra Gaúcha.</p>
+          <p>Nosso objetivo e prioridade é a qualidade e expressão artística de nossos produtos. Preferimos não
+            divulgar safras, passagem por madeira ou formas específicas de elaboração dos nossos fermentados,
+            pois queremos que o consumidor nos escolha pela qualidade e confiança na marca e não no marketing
+            tradicional atrelado aos estigmas do vinho. Somos iconoclastas da sofisticação enológica
+          </p>
+        </div>
+      </div>
+    </AboutStyle>
+  )
+}
+
+export default About
